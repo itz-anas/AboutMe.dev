a2bcd502cabaa6460a2c05f781d025c724640c7a
itz-anas <mohammedanashere@gmail.com>
2025-06-14 17:02:41 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AboutMe.dev/", 
+  base: "/AboutMe.dev/", // Match your GitHub repo name exactly
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
-
-server: {
-  host: true,
-  port: 3000
-}
